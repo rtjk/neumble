8750a80876bb086b6876cc7ad1b84289da4cbfce
rtjk <>
2021-01-09 16:34:15 -0500

    added body_temperature constant

--- neumble.py
+++ neumble.py
@@ -2,7 +2,8 @@ import numpy as np
 
 R = 8.314 # universal gas constant [ J * K^(-1) * mol^(-1) ]
 F = 96485 # Faraday constant [ C * mol^(-1) ]
+body_temperature = 310 # [K]
 
-# compute the nernst potetial (aka equilibrium potential, reversal potential) for a given ion
-def nernst_simple(valence, concentration_out, concentration_in, temperature=310):
+# compute the nernst potetial (aka equilibrium potential, reversal potential) for a given ion [V]
+def nernst_simple(valence, concentration_out, concentration_in, temperature=body_temperature):
     return R * temperature * ( valence * F )**(-1) * np.log( concentration_out / concentration_in )
