e81da1edc0de63e619e77efd92ca3ca856108ded
rtjk <47841774+rtjk@users.noreply.github.com>
2021-01-09 16:50:15 -0500

    test commit with new user email

--- neumble.py
+++ neumble.py
@@ -7,3 +7,5 @@ body_temperature = 310 # [K]
 # compute the nernst potetial (aka equilibrium potential, reversal potential) for a given ion [V]
 def nernst_simple(valence, concentration_out, concentration_in, temperature=body_temperature):
     return R * temperature * ( valence * F )**(-1) * np.log( concentration_out / concentration_in )
+
+# test
