67108300298dc788d126844ac542aa4ff6da708c
rtjk <>
2021-01-09 15:55:33 -0500

    Nernst equation

--- neumble.py
+++ neumble.py
@@ -1,1 +1,8 @@
-#test
+import numpy as np
+
+R = 8.314 # universal gas constant [ J * K^(-1) * mol^(-1) ]
+F = 96485 # Faraday constant [ C * mol^(-1) ]
+
+# compute the nernst potetial (aka equilibrium potential, reversal potential) for a given ion
+def nernst_simple(valence, concentration_out, concentration_in, temperature=310):
+    return R * temperature * ( valence * F )**(-1) * np.log( concentration_out / concentration_in )
