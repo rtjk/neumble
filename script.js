3848f23986313220f5a3bb1bb19f7d86d58a5c51
rtjk <47841774+rtjk@users.noreply.github.com>
2021-01-21 21:18:53 -0500

    refactoring and variable particle speed

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ console.log(ctx);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-const lmElement = document.getElementById("c");
+const lmElement = document.getElementById("c1");
 let lmBounds = lmElement.getBoundingClientRect();
 let lm = {
   x: lmBounds.left,
@@ -13,7 +13,9 @@ let lm = {
   height: lmBounds.height
 };
 
-const rmElement = document.getElementById("d");
+let g = false;
+
+const rmElement = document.getElementById("c2");
 let rmBounds = rmElement.getBoundingClientRect();
 let rm = {
   x: rmBounds.left,
@@ -22,13 +24,21 @@ let rm = {
   height: rmBounds.height
 };
 
-let numberOfParticles = 300;
+const cmElement = document.getElementById("c3");
+let cmBounds = cmElement.getBoundingClientRect();
+let cm = {
+  x: cmBounds.left,
+  y: cmBounds.top,
+  width: cmBounds.width,
+  height: cmBounds.height
+};
+
+let numberOfParticles = 500;
 let particlesArray = [];
 const speed = 4;
 
 class Particle {
   constructor() {
-    
     this.radius = 10;
 
     this.ix = Math.random() * canvas.width;
@@ -39,27 +49,18 @@ class Particle {
     do {
       this.ix = Math.random() * canvas.width;
       this.iy = Math.random() * canvas.height;
-    } while (
-      (this.ix - this.radius < lm.x + lm.width &&
-      this.ix + this.radius > lm.x &&
-      this.iy - this.radius < lm.y + lm.height &&
-      this.iy + this.radius > lm.y) ||
-      (this.ix - this.radius < rm.x + rm.width &&
-      this.ix + this.radius > rm.x &&
-      this.iy - this.radius < rm.y + rm.height &&
-      this.iy + this.radius > rm.y)
-    );
-    
+    } while (collision(this.ix, this.iy, this.radius));
 
     this.x = this.ix;
     this.y = this.iy;
     this.speedX = Math.random() * speed - 1.5;
     this.speedY = Math.random() * speed - 1.5;
+
+    this.color = "white";
+    if (this.iy > 0.5 * canvas.height) this.color = "green";
   }
   draw() {
-    ctx.fillStyle = "white";
-    if(this.iy > .5 * canvas.height)
-      ctx.fillStyle = "green";
+    ctx.fillStyle = this.color;
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.closePath();
@@ -76,19 +77,23 @@ class Particle {
     if (this.y + this.radius > canvas.height || this.y + this.radius < 0) {
       this.speedY = -this.speedY;
     }
-    if (
-      (this.x - this.radius < lm.x + lm.width &&
-      this.x + this.radius > lm.x &&
-      this.y - this.radius < lm.y + lm.height &&
-      this.y + this.radius > lm.y) ||
-      (this.x - this.radius < rm.x + rm.width &&
-      this.x + this.radius > rm.x &&
-      this.y - this.radius < rm.y + rm.height &&
-      this.y + this.radius > rm.y)
-    ) {
+    if (collision(this.x, this.y, this.radius)) {
       this.speedX = -this.speedX;
       this.speedY = -this.speedY;
     }
+
+    if (this.color == "green" && g) {
+      this.color = "red";
+      this.speedX *= 5;
+      this.speedY *= 5;
+    }
+
+    if (this.color == "red" && !g) {
+      this.color = "green";
+      this.speedX /= 5;
+      this.speedY /= 5;
+    }
+
     this.draw();
   }
 }
@@ -111,9 +116,40 @@ init();
 animate();
 
 window.addEventListener("resize", function () {
+  reset();
+});
+
+document.getElementById("g").onclick = changeWeight;
+document.getElementById("r").onclick = reset;
+
+function changeWeight() {
+  g = !g;
+}
+
+function reset() {
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
   particlesArray = [];
   init();
-});
+}
+
+function collision(x, y, radius) {
+  if (
+    (x - radius < lm.x + lm.width &&
+      x + radius > lm.x &&
+      y - radius < lm.y + lm.height &&
+      y + radius > lm.y) ||
+    (x - radius < rm.x + rm.width &&
+      x + radius > rm.x &&
+      y - radius < rm.y + rm.height &&
+      y + radius > rm.y) ||
+    (x - radius < cm.x + cm.width &&
+      x + radius > cm.x &&
+      y - radius < cm.y + cm.height &&
+      y + radius > cm.y)
+  ) {
+    return true;
+  }
+  return false;
+}
 
